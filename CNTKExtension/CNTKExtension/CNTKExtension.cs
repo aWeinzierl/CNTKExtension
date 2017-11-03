047b39603e1a924ca84463424275c71b64beec0f
Andreas Weinzierl <aWeinzierl@users.noreply.github.com>
2017-11-03 19:20:48 +0100

    allow custom weightInitializer

--- CNTKExtension/CNTKExtension/CNTKExtension.cs
+++ CNTKExtension/CNTKExtension/CNTKExtension.cs
@@ -9,49 +9,49 @@ namespace CNTK
 {
     public static class CNTKExtension
     {
-        public static Function Dense(Variable operand, int outputDim, DeviceDescriptor device)
+        public static Function Dense(Variable operand, int outputDim, CNTKDictionary weightInitializer, DeviceDescriptor device)
         {
             if (operand.Shape.Rank == 1)
-                return FullyConnectedLinearLayer(operand, outputDim, device);
+                return FullyConnectedLinearLayer(operand, outputDim, device, weightInitializer);
 
             //flatten input layer
             var newDim = operand.Shape.Dimensions.Aggregate((d1, d2) => d1 * d2);
             operand = CNTKLib.Reshape(operand, new int[] { newDim });
 
-            return FullyConnectedLinearLayer(operand, outputDim, device);
+            return FullyConnectedLinearLayer(operand, outputDim, device, weightInitializer);
         }
 
-        public static Function Dense(Variable operand, int outputDim, DeviceDescriptor device, string name)
+        public static Function Dense(Variable operand, int outputDim, CNTKDictionary weightInitializer, DeviceDescriptor device, string name)
         {
-            //flatten input layer if necessary
+            //flatten input layer
             if (operand.Shape.Rank == 1)
-                return FullyConnectedLinearLayer(operand, outputDim, device, name);
+                return FullyConnectedLinearLayer(operand, outputDim, device, weightInitializer, name);
 
             var newDim = operand.Shape.Dimensions.Aggregate((d1, d2) => d1 * d2);
             operand = CNTKLib.Reshape(operand, new[] { newDim });
 
-            return FullyConnectedLinearLayer(operand, outputDim, device, name);
+            return FullyConnectedLinearLayer(operand, outputDim, device, weightInitializer, name);
         }
 
         private static Function FullyConnectedLinearLayer(Variable input, int outputDim, DeviceDescriptor device,
-            string name = "")
+            CNTKDictionary weightIntializer, string name = "")
         {
             System.Diagnostics.Debug.Assert(input.Shape.Rank == 1);
 
             var inputDim = input.Shape[0];
 
             int[] weightMatrixDimensions = { outputDim, inputDim };
-            var weights = new Parameter(weightMatrixDimensions, DataType.Float,
-                CNTKLib.GlorotUniformInitializer(
-                    CNTKLib.DefaultParamInitScale,
-                    CNTKLib.SentinelValueForInferParamInitRank,
-                    CNTKLib.SentinelValueForInferParamInitRank, 1),
-                device, "weights");
+            var weights = new Parameter(
+                weightMatrixDimensions,
+                DataType.Float,
+                weightIntializer,
+                device,
+                "weights");
             var timesFunction = CNTKLib.Times(weights, input, "times");
 
             int[] biasDimension = { outputDim };
-
             var bias = new Parameter(biasDimension, 0.0f, device, "plusParam");
+
             return CNTKLib.Plus(bias, timesFunction, name);
         }
 
